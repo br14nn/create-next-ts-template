24e526405c0257ac345419045f7c5ca41d09b753
br14nn <vituallabrian727@gmail.com>
2023-12-20 23:15:32 +0800

    removed some shell commands

--- bin/index.js
+++ bin/index.js
@@ -5,24 +5,16 @@ const runCommand = (command) => {
   try {
     execSync(`${command}`, { stdio: "inherit" });
   } catch (error) {
-    console.error(`Failed to execute ${command}`, e);
+    console.error(`Failed to execute ${command}`, error);
     return false;
   }
   return true;
 };
 
 const repoName = process.argv[2];
-const updateCommand = `npm update -g create-next-ts-template`;
 const gitCheckoutCommand = `git clone https://github.com/br14nn/next-ts-template.git ${repoName}`;
-const installDepsCommand = `cd ${repoName} && npm i`;
+const installDepsCommand = `cd ${repoName} && npm install`;
 const deleteGitFolderCommand = `cd ${repoName} && rmdir /s /q .git`;
-const removeOriginUrlCommand = `cd ${repoName} && git remote remove origin`;
-
-console.log(`Updating create-next-ts-template`);
-const update = runCommand(updateCommand);
-if (!update) process.exit(-1);
-
-console.log("");
 
 console.log(`Cloning the repository with name ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
@@ -31,22 +23,19 @@ if (!checkedOut) process.exit(-1);
 console.log("");
 
 console.log(`Installing dependencies for ${repoName}`);
-const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) process.exit(-1);
+const installDeps = runCommand(installDepsCommand);
+if (!installDeps) process.exit(-1);
 
-console.log("");
+console.log(-1);
 
-console.log(`Performing "rmdir /s /q .git`);
+console.log(`Deleting .git folder in ${repoName} folder`);
 const deleteGitFolder = runCommand(deleteGitFolderCommand);
 if (!deleteGitFolder) process.exit(-1);
 
 console.log("");
 
-console.log(`Performing "git remote remove origin"`);
-const removeOriginUrl = runCommand(removeOriginUrlCommand);
-if (!removeOriginUrl) process.exit(-1);
+console.log("Congratulations! You are ready. Follow the following commands to start:");
 
 console.log("");
 
-console.log("Congratulations! You are ready. Follow the following commands to start:");
 console.log(`cd ${repoName} && npm run dev`);
