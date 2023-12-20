d122823efbb73243f8028ef57e5a66ab0dcb39b3
br14nn <vituallabrian727@gmail.com>
2023-12-20 22:07:27 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# About
+
+The npx tool for the "next-ts-template" repository
+
+# Installation
+
+Step 1:
+
+```sh
+npm install -g create-next-ts-template
+```
+
+Step 2:
+
+```sh
+npx create-next-ts-template your-app-name
+```
---
+++ bin/index.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+const { execSync } = require("child_process");
+
+const runCommand = (command) => {
+  try {
+    execSync(`${command}`, { stdio: "inherit" });
+  } catch (error) {
+    console.error(`Failed to execute ${command}`, e);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone https://github.com/br14nn/next-ts-template.git ${repoName}`;
+const installDepsCommand = `cd ${repoName} && git remote remove origin && npm i`;
+
+console.log(`Cloning the repository with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if (!checkedOut) process.exit(-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+const installedDeps = runCommand(installDepsCommand);
+if (!installedDeps) process.exit(-1);
+
+console.log("");
+console.log("Congratulations! You are ready. Follow the following commands to start:");
+console.log(`cd ${repoName} && npm run dev`);
